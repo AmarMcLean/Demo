1cd80facb7361a9409cdfc9d6629e27b932428d2
Sean Henderson <91705235+sean-henderson@users.noreply.github.com>
2023-05-03 22:11:44 -0400

    Add files via upload
    Coin entity commit

---
+++ src/js/renderables/coin.js
@@ -0,0 +1,29 @@
+import { Collectable, Entity, game, input, Rect, sprite, collision, audio, level } from 'melonjs';
+
+class CoinEntity extends Collectable {
+    // constructor
+    constructor(x, y, settings) {
+        // call the parent constructor
+        super(x, y , settings);
+
+        // this item collides ONLY with PLAYER_OBJECT
+        this.body.setCollisionMask(collision.types.PLAYER_OBJECT);
+    }
+
+
+    // handles the removal of a coin when player collides with it
+    onCollision(response, other) {
+        // do something when collected
+
+        // make sure it cannot be collected "again"
+        this.body.setCollisionMask(collision.types.NO_OBJECT);
+
+        // remove it and play a sound
+        audio.play("cling")
+        game.world.removeChild(this);
+
+        return false
+    }
+}
+
+export default CoinEntity;
